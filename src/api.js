43213dcf4f4d89df146996421ec26e200ea6fd92
m-lukas <lukas.mueller.gr@t-online.de>
2018-10-31 17:40:28 +0100

    Update api.js (header description)

--- src/api.js
+++ src/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const API_URL = "http://127.0.0.1:8000/api";
+const API_URL = "http://127.0.0.1:8000/api"; //TODO: Add dotenv support (-> webpack config)
 
 //structured API requests (e.g. api.songs.search(data).then(searchResult => ...))
 export default {
     songs:
         {
-            search: header =>
-                axios.get(`${API_URL}/search`, {headers: header}).then(res => res.data),
+            search: query =>
+                axios.get(`${API_URL}/search`, {headers: query}).then(res => res.data),
             get: id =>
                 axios.get(`${API_URL}/api/songs/get/${id}`).then(res => res.data),
             transitions: id =>
@@ -18,15 +18,15 @@ export default {
             get: id =>
                 axios.get(`${API_URL}/playlist/${id}`).then(res => res.data),
             create: data => 
-                axios.post(`${API_URL}/playlist`, { data }).then(res => res.data),
+                axios.post(`${API_URL}/playlist`, {headers: data}).then(res => res.data),
             put: id => 
                 axios.put(`${API_URL}/playlist/${id}`).then(res => res.data),
         },
     auth: 
         {
             login: credentials =>
-                axios.post(`${API_URL}/auth`, { credentials }).then(res => res.data),
+                axios.post(`${API_URL}/auth`, {headers: credentials}).then(res => res.data),
             register: user =>
-                axios.put(`${API_URL}/auth`, { user }).then(res => res.data),    
+                axios.put(`${API_URL}/auth`, {headers: user}).then(res => res.data),    
         }
 }
