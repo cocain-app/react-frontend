f3ce87de46950b64ead98f239e80e7c35a797512
Ben Bachem <10088265+benbachem@users.noreply.github.com>
2018-11-01 11:13:40 +0100

    Split up storybook parts

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,7 +2,50 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Playlist', module)
-  .add('Empty Playlist', () => (
-    <div>Test</div>
+// Lukas
+storiesOf('Searchbar', module)
+  .add('default', () => (
+    <div></div>
+  ))
+
+// Lukas
+storiesOf('Search Table', module)
+  .add('default', () => (
+    <div></div>
+  ))
+
+// Lukas
+storiesOf('Song Row', module)
+  .add('default', () => (
+    <div></div>
+  ))
+
+// Ben
+storiesOf('Playlist Head', module)
+  .add('default', () => (
+    <div></div>
+  ))
+
+// Ben
+storiesOf('Playlist Table', module)
+  .add('default', () => (
+    <div></div>
+  ))
+
+// Ben
+storiesOf('Recomendations', module)
+  .add('default', () => (
+    <div></div>
+  ))
+
+// Ben
+storiesOf('Reccomendation', module)
+  .add('default', () => (
+    <div></div>
+  ))
+
+// Ben
+storiesOf('Dropdown', module)
+  .add('default', () => (
+    <div></div>
   ))
