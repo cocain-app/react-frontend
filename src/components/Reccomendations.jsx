b592904890e2aeac8eb9cd49ebe6773c87ab5e51
Ben Bachem <10088265+benbachem@users.noreply.github.com>
2018-11-10 17:39:51 +0100

    Implemented Placeholders

--- src/components/Reccomendations.jsx
+++ src/components/Reccomendations.jsx
@@ -35,31 +35,43 @@ class Reccomendations extends Component {
   render() {
     return (
       <div className="Reccomendations_wrapper">
-        <div className="Reccomendations">
-          <h2>
-            <p>Recomended Songs based on <a href="#">{this.props.base}</a></p>
-            { this.state.expanded ? (
-              <span className="close" onClick={() => this.collapse()}>
-                <i class="material-icons">expand_more</i>
-              </span>
-            ) : (
-              <span className="expand" onClick={() => this.expand()}>
-                <i class="material-icons">expand_less</i>
-              </span>
-            ) }
-          </h2>
 
-          { this.state.expanded ? (
-            <div className="wrapper">
-              <div
-                onScroll={(e) => this.onScroll(e)}
-                className={`view ${this.state.remainingScroll < 300 ? "no-fade" : "fade"}`}>
-                { this.props.children }
-              </div>
+        {(this.props.children === undefined || (this.props.children && this.props.children.length === 0)) && (
+          <div className="Reccomendations">
+            <div className="placeholder">
+              <h2>No reccomendations found</h2>
+              <p>Unfortuneatly we don't have any record of fitting songs.</p>
             </div>
-          ) : (<div />) }
+          </div>
+        )}
 
-        </div>
+        {this.props.children !== undefined && (
+          <div className="Reccomendations">
+            <h2>
+              <p>Recomended Songs based on <a href="#">{this.props.base}</a></p>
+              { this.state.expanded ? (
+                <span className="close" onClick={() => this.collapse()}>
+                  <i class="material-icons">expand_more</i>
+                </span>
+              ) : (
+                <span className="expand" onClick={() => this.expand()}>
+                  <i class="material-icons">expand_less</i>
+                </span>
+              ) }
+            </h2>
+
+            { this.state.expanded ? (
+              <div className="wrapper">
+                <div
+                  onScroll={(e) => this.onScroll(e)}
+                  className={`view ${this.state.remainingScroll < 300 ? "no-fade" : "fade"}`}>
+                  { this.props.children }
+                </div>
+              </div>
+            ) : (<div />) }
+
+          </div>
+        )}
       </div>
     )
   }
--- src/components/pages/SearchPage.jsx
+++ src/components/pages/SearchPage.jsx
@@ -11,26 +11,40 @@ class SearchPage extends Component {
   render() {
     return (
       <div className="Search Page">
-        <div className="container">
-          <SongTable headers={["Track", <i class="material-icons">schedule</i>, "BPM", "Key"]}>
-            <AppContext.Consumer>
-              {(context) => Object.values(context.search.suggestions).map((suggestion) => (
-                <SongRow
-                  key={suggestion.Song.ID}
-                  selected={false}
-                  type={context.playlist.songs.length > 0 ? "add" : "create" }
-                  id={suggestion.Song.ID}
-                  artist={suggestion.Song.Artist}
-                  bpm={suggestion.Song.BPM}
-                  musicalKey={suggestion.Song.Key}
-                  duration={suggestion.Song.Duration}
-                  coverURL={suggestion.Song.ImageURLSmall}
-                  previewUrl={suggestion.Song.PreviewURL}
-                  title={suggestion.Song.Title} />
-              ))}
-            </AppContext.Consumer>
-          </SongTable>
-        </div>
+        <AppContext.Consumer>
+          {(context) => (
+            <React.Fragment>
+              {context.search.suggestions.length === 0 && (
+                <div className="placeholder">
+                  <h2>Search Cocain</h2>
+                  <p>Try looking for songs or artists.</p>
+                </div>
+              )}
+
+              {context.search.suggestions.length > 0 && (
+                <div className="container">
+                  <SongTable headers={[
+                    "Track", <i className="material-icons">schedule</i>, "BPM", "Key"]}>
+                    { Object.values(context.search.suggestions).map((suggestion) => (
+                      <SongRow
+                        key={suggestion.Song.ID}
+                        selected={false}
+                        type={context.playlist.songs.length > 0 ? "add" : "create" }
+                        id={suggestion.Song.ID}
+                        artist={suggestion.Song.Artist}
+                        bpm={suggestion.Song.BPM}
+                        musicalKey={suggestion.Song.Key}
+                        duration={suggestion.Song.Duration}
+                        coverURL={suggestion.Song.ImageURLSmall}
+                        previewUrl={suggestion.Song.PreviewURL}
+                        title={suggestion.Song.Title} />
+                    ))}
+                  </SongTable>
+                </div>
+              )}
+            </React.Fragment>
+          )}
+        </AppContext.Consumer>
       </div>
     )
   }
